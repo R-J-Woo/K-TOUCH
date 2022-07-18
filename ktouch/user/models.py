9c43f8617273e2c98adfa4330eadb6ca385bd37a
Jewoo Ryu <jewoo20000624@gmail.com>
2022-07-18 18:00:48 +0900

    메인페이지 이미지, 문의 게시판 질문 수정

--- ktouch/user/models.py
+++ ktouch/user/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class User(models.Model):
-    uid = models.CharField(verbose_name='아이디', max_length=64, unique=True)
-    password = models.CharField(verbose_name='비밀번호', max_length=64)
+    uid = models.CharField(verbose_name='아이디', max_length=16, unique=True)
+    password = models.CharField(verbose_name='비밀번호', max_length=200)
     phone_number = models.CharField(verbose_name='전화 번호', max_length=16)
-    name = models.CharField(verbose_name='이름', max_length=64)
+    name = models.CharField(verbose_name='이름', max_length=16)
     register_date = models.DateTimeField(
         verbose_name='가입 날짜', auto_now_add=True)
 
